c554f677e5ede704f077c3672fb33c6d3a5e153d
jpemberthy <jpemberthy@gmail.com>
2012-03-08 16:00:55 -0500

    Aria request default params.

--- lib/aria_billing.rb
+++ lib/aria_billing.rb
@@ -5,8 +5,14 @@ require "aria_billing/configuration"
 module AriaBilling
   include HTTParty
 
-  def self.make_request(opts)
-    opts.merge! AriaBilling::Configuration.credentials
+  def self.make_request(opts={})
+    opts = request_defaults.merge opts
     post(AriaBilling::Configuration.url, body: opts)
   end
+
+  def self.request_defaults
+    { output_format: "json" }.merge AriaBilling::Configuration.credentials
+  end
+
+
 end
--- spec/aria_billing_spec.rb
+++ spec/aria_billing_spec.rb
@@ -3,11 +3,31 @@ require 'spec_helper'
 describe AriaBilling do
   describe "self.make_request(params)" do
     it "makes a post to the configuration URL with the credentails and params in the body" do
-      AriaBilling.should_receive(:post).
-        with("url", body: { auth_key: "auth_key", client_no: "client_no", message: "hello", user: "tony"}).
+      aria.should_receive(:post).
+        with("url", body: { auth_key: "auth_key", client_no: "client_no", message: "hello", user: "tony", output_format: "json" }).
           and_return("response")
 
-      AriaBilling.make_request(message: "hello", user: "tony")
+      aria.make_request(message: "hello", user: "tony")
+    end
+
+    it "responds with json by default" do
+      aria.should_receive(:post).
+        with("url", body: { auth_key: "auth_key", client_no: "client_no", output_format: "json" }).
+          and_return("response")
+
+      aria.make_request
+    end
+
+    it "rewrites params passed as opts" do
+      aria.should_receive(:post).
+        with("url", body: { auth_key: "the-special-key", output_format: "xml", client_no: "client_no" }).
+          and_return("response")
+
+      aria.make_request(auth_key: "the-special-key", output_format: "xml")
+    end
+
+    def aria
+      AriaBilling
     end
   end
 end
