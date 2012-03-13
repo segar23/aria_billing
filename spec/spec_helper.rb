37b04a4ed35ab38734b9c94beb746edf1444d053
jpemberthy <jpemberthy@gmail.com>
2012-03-12 21:33:31 -0500

    Adds rspec metadata support for vcr.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,11 @@ VCR.configure do |c|
   current_dir = File.dirname(__FILE__)
   c.cassette_library_dir = "#{current_dir}/fixtures/vcr_cassettes"
   c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  # so we can use `:vcr` rather than `:vcr => true`;
+  # in RSpec 3 this will no longer be necessary.
+  c.treat_symbols_as_metadata_keys_with_true_values = true
 end
