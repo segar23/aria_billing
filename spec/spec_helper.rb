3e3f13aeeccec898872839fec043c4303829ecd7
jpemberthy <jpemberthy@gmail.com>
2012-03-12 21:18:43 -0500

    VCR integration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require "aria_billing"
+require "vcr"
 
 AriaBilling::Configuration.auth_key = "auth_key"
 AriaBilling::Configuration.client_no = "client_no"
 AriaBilling::Configuration.url = "url"
+
+VCR.configure do |c|
+  current_dir = File.expand_path File.dirname(__FILE__)
+  c.cassette_library_dir = "#{current_dir}/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+end
