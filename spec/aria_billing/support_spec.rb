e351b7e667e8d729e1da2ec5248783a6320cf249
jpemberthy <jpemberthy@gmail.com>
2012-03-15 15:25:55 -0500

    refactors support_spec.rb

--- spec/aria_billing/support_spec.rb
+++ spec/aria_billing/support_spec.rb
@@ -18,22 +18,20 @@ describe AriaBilling::Support do
   end
 
   describe "self.gen_random_string(params)",:vcr do
-    it "generates a random string" do 
-      response= AriaBilling::Support.gen_random_string({"rand_type" => 'A',
-                                                        "rand_length" => 5,
-                                                        "rand_case" => 'U'})
+    it "generates a random string" do
+      params = { "rand_type" => 'A', "rand_length" => 5, "rand_case" => 'U' }
+      response= AriaBilling::Support.gen_random_string params
       response["random_string"].should_not be_nil
       response["random_string"].should have(5).caracters
       response["random_string"].should match(/^[^a-z]*$/)
     end
 
-    it "returns errors" do 
-       response= AriaBilling::Support.gen_random_string({"rand_type" => 'K',
-                                                        "rand_length" => 5,
-                                                        "rand_case" => 'P'})
-       response["random_string"].should be_nil
-       response["error_msg"].should == "invalid input"
-    end  
+    it "returns errors" do
+      params = {"rand_type" => 'K',"rand_length" => 5, "rand_case" => 'P'}
+      response = AriaBilling::Support.gen_random_string params
+      response["random_string"].should be_nil
+      response["error_msg"].should == "invalid input"
+    end
   end
 
   describe "self.get_client_countries(params)",:vcr do
@@ -50,7 +48,7 @@ describe AriaBilling::Support do
       response["client_country"][1]["country_native"].should == "United States"
       response["client_country"][1]["country_english"].should == "United States"
       response["client_country"][1]["iso_3166_1n"].should == "840"
-      response["client_country"][1]["currency_cd"].should == "usd"      
+      response["client_country"][1]["currency_cd"].should == "usd"
     end
   end
 end
