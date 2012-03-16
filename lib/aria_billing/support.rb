77b9d1b0282451c7ab541541d4ad7d2356ca9422
Sebastian Villa <svilla@psl.com.co>
2012-03-16 14:35:57 -0500

    add some test

--- lib/aria_billing/support.rb
+++ lib/aria_billing/support.rb
@@ -4,7 +4,10 @@ module AriaBilling
                   'userid_exists','userid_is_available','validate_session',
                   'get_web_replacement_vals','reset_usg_mtd_bal','reset_usg_ptd_bal',
                   'set_acct_usg_mtd_threshold','set_acct_usg_ptd_threshold','set_client_usg_mtd_threshold',
-                  'set_client_usg_ptd_threshold','get_unbilled_usage_summary','create_acct_complete']
+                  'set_client_usg_ptd_threshold','get_unbilled_usage_summary','create_acct_complete',
+                  'adjust_billing_dates','assign_collections_acct_group','assign_functional_acct_group',
+                  'assign_supp_plan','ban_acct','cancel_acct_plan_contract','cancel_supp_plan',
+                  'close_acct_installation','create_acct_plan_contract','modify_acct_plan_contract']
 
     class << self
       API_CALLS.each do |call_name|
--- spec/aria_billing/support_spec.rb
+++ spec/aria_billing/support_spec.rb
@@ -1,17 +1,6 @@
 require 'spec_helper'
 
 describe AriaBilling::Support do
-  describe "self.get_country_from_ip(params)", :vcr do
-    it "returns the country code and name of a given IP" do
-      response = AriaBilling::Support.get_country_from_ip("ip_address" => "64.71.167.222")
-
-      response.should have_key("country_code")
-      response.should have_key("country_name")
-      response.should have_key("error_code")
-      response.should have_key("error_msg")
-
-    end
-  end
 
   describe "self.gen_random_string(params)",:vcr do
     it "generates a random string" do
@@ -160,7 +149,7 @@ describe AriaBilling::Support do
     end
   end
 
-# Account Modification
+# Account Creation
 
   describe "self.create_acct_complete(params)",:vcr do
     it "Creates an account with an extensive set of data elements" do
@@ -184,4 +173,129 @@ describe AriaBilling::Support do
 
     end
   end
+
+#Account Modification
+
+  describe "self.adjust_billing_dates(params)",:vcr do 
+    it "Adjust the billing date for a specified account to a date in the future or date in the past" do
+      params = { "acct_no" => 1, "action_directive" => 1, "adjustament_days" => 1,"comments" => "Test" }
+      response = AriaBilling::Support.adjust_billing_dates params
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.assign_collections_acct_group(params)",:vcr do
+    it "Assigns a specified account to a collections account group" do
+      response = AriaBilling::Support.assign_collections_acct_group({ "acct_no" => 1, "group_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.assign_functional_acct_group(params)",:vcr do
+    it "Assigns a specified account to a functional account group" do
+      response = AriaBilling::Support.assign_functional_acct_group({ "acct_no" => 1, "group_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.assign_supp_plan(params)",:vcr do
+    it "Assigns one supplemental plan to a specified account" do
+      response = AriaBilling::Support.assign_supp_plan({ "acct_no" => 1, "supp_plan_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+      response.should have_key("proration_result_amount")
+      response.should have_key("invoice_no")
+      response.should have_key("supp_plan_line_items")
+      response.should have_key("total_charges_before_tax")
+      response.should have_key("total_tax_charges")
+      response.should have_key("total_credit")
+
+    end
+  end
+
+  describe "self.ban_acct(params)",:vcr do
+    it "Sets an account's status to temporary ban" do
+      response = AriaBilling::Support.ban_acct({ "acct_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.cancel_acct_plan_contract(params)",:vcr do
+    it "Cancels a contract assigned to a specified account holder" do
+      response = AriaBilling::Support.cancel_acct_plan_contract({ "acct_no" => 1, "contract_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.cancel_supp_plan(params)",:vcr do
+    it "Cancels the supplemental plans assigned to a specified account" do
+      response = AriaBilling::Support.cancel_supp_plan({ "acct_no" => 1, "supp_plan_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+      response.should have_key("proration_result_amount")
+      response.should have_key("invoice_no")
+
+    end
+  end
+
+  describe "self.close_acct_installation(params)",:vcr do
+    it "For accounts in the pending installation status, marks the installation" do
+      response = AriaBilling::Support.close_acct_installation({ "acct_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.create_acct_plan_contract(params)",:vcr do
+    it "Creates a contract for a specified account holder and plan" do
+      response = AriaBilling::Support.create_acct_plan_contract({ "acct_no" => 1, "plan_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+      response.should have_key("contract_no")
+
+    end
+  end
+
+  describe "self.get_country_from_ip(params)", :vcr do
+    it "returns the country code and name of a given IP" do
+      response = AriaBilling::Support.get_country_from_ip("ip_address" => "64.71.167.222")
+
+      response.should have_key("country_code")
+      response.should have_key("country_name")
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
+  describe "self.modify_acct_plan_contract(params)",:vcr do
+    it "modifies a particular contract assigned to a specified account" do
+      response = AriaBilling::Support.modify_acct_plan_contract({ "acct_no" => 1, "contract_no" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+
+    end
+  end
+
 end
