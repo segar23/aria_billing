b3f7d1ce6c06c32b924840ba94a02025578e3a57
Sebastian Villa <svilla@psl.com.co>
2012-03-20 12:07:10 -0500

    added update_esisting_billing_info test

--- lib/aria_billing/support.rb
+++ lib/aria_billing/support.rb
@@ -13,7 +13,7 @@ module AriaBilling
                   'set_pay_method_bank_draft', 'set_pay_method_cc','set_pay_method_net_terms',
                   'set_payment_responsibility','update_acct_complete','update_acct_contact',
                   'update_acct_credentials','update_acct_notify','update_acct_status',
-                  'update_supp_fields','update_existing_billing_info']
+                  'update_supp_fields','update_existing_billing_info','get_acct_comments']
 
     class << self
       API_CALLS.each do |call_name|
