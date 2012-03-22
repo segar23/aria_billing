6b5846e5fc02fa84d06855b3eb03f12e71b305ed
Sebastian Villa <svilla@psl.com.co>
2012-03-22 08:41:09 -0500

    added get_acct_message test

--- spec/aria_billing/api/account_communucation_retrieval_spec.rb
+++ spec/aria_billing/api/account_communucation_retrieval_spec.rb
@@ -7,12 +7,23 @@ describe "Account Comunication Retrieval" do
   end
 
   describe "self.get_acct_invoice_history(params)",:vcr do 
-    it "Adjust the billing date for a specified account to a date in the future or date in the past" do
+    it "Returns the invoice history for a specified account" do
       response = api.get_acct_invoice_history ({ "acct_no" => 1 })
 
       response.should have_key("error_code")
       response.should have_key("error_msg")
       response.should have_key("invoice_history")
+    end
+  end
+
+  describe "self.get_acct_message(params)",:vcr do 
+    it "Returns the body of a particular email message sent to a specified account holder" do
+      response = api.get_acct_message ({ "acct_no" => 1, "message_id" => 1 })
+
+      response.should have_key("error_code")
+      response.should have_key("error_msg")
+      response.should have_key("message_body")
+      response.should have_key("mime_type")
     end
   end
 end
